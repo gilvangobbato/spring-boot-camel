474ff17e9556c41fdb06454a17fbe99a7af71cf8
Gilvan André Gobbato <gilvangobbato@gmail.com>
2020-12-16 21:29:00 -0300

    Adicionando docker-compose

--- spring-boot-service/src/main/java/com/github/gilvangobbato/presentation/ApiController.java
+++ spring-boot-service/src/main/java/com/github/gilvangobbato/presentation/ApiController.java
@@ -1,17 +1,24 @@
 package com.github.gilvangobbato.presentation;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@RestController(value = "/v1")
+@Log4j2
+@RestController
+@RequestMapping("/servico")
 public class ApiController {
 
-    @GetMapping()
+    @GetMapping(produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> returnTime(){
-        return ResponseEntity.ok(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+        log.error(time);
+        return ResponseEntity.ok(time);
     }
 }
